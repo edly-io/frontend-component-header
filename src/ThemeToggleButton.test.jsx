645957b9fb62c68b2a83915f399862e9a8a0ab7d
hinakhadim <hina.khadim@arbisoft.com>
2025-05-06 11:44:56 +0500

    chore: resolves lint issues

--- src/ThemeToggleButton.test.jsx
+++ src/ThemeToggleButton.test.jsx
@@ -2,55 +2,51 @@
 import React from 'react';
 import { IntlProvider } from '@edx/frontend-platform/i18n';
 import { render, fireEvent } from '@testing-library/react';
-import ThemeToggleButton from './ThemeToggleButton';
 import { getConfig } from '@edx/frontend-platform';
-
+import ThemeToggleButton from './ThemeToggleButton';
 
 jest.mock('@edx/frontend-platform', () => ({
-    getConfig: jest.fn(),
+  getConfig: jest.fn(),
 }));
 
 const mockCookiesGet = jest.fn();
 const mockCookiesSet = jest.fn();
-jest.mock('universal-cookie', () => {
-    return jest.fn().mockImplementation(() => ({
-        get: mockCookiesGet, // Simulate initial light mode
-        set: mockCookiesSet,
-    }));
-});
+jest.mock('universal-cookie', () => jest.fn().mockImplementation(() => ({
+  get: mockCookiesGet, // Simulate initial light mode
+  set: mockCookiesSet,
+})));
 
 describe('ThemeToggleButton', () => {
-    beforeEach(() => {
-        
-        getConfig.mockReturnValue({
-            LMS_BASE_URL: 'https://fake.url',
-            INDIGO_ENABLE_DARK_TOGGLE: true,
-        });
-
-        // Reset body class
-        document.body.classList.remove('indigo-dark-theme');
+  beforeEach(() => {
+    getConfig.mockReturnValue({
+      LMS_BASE_URL: 'https://fake.url',
+      INDIGO_ENABLE_DARK_TOGGLE: true,
     });
 
-    it('calls onToggleTheme when Enter key is pressed', () => {
-        mockCookiesGet.mockReturnValue('light');
+    // Reset body class
+    document.body.classList.remove('indigo-dark-theme');
+  });
 
-        const { container } = render(
-            <IntlProvider locale="en" messages={{}}>
-                <ThemeToggleButton />
-            </IntlProvider>
-        );
-        const checkbox = container.querySelector('#theme-toggle-checkbox');
-        checkbox.focus();
-        fireEvent.keyUp(checkbox, { key: 'Enter' });
+  it('calls onToggleTheme when Enter key is pressed', () => {
+    mockCookiesGet.mockReturnValue('light');
 
-        expect(mockCookiesSet).toHaveBeenCalledWith(
-            'indigo-toggle-dark',
-            'dark',
-            expect.objectContaining({
-                domain: 'fake.url',
-                path: '/',
-                expires: expect.any(Date)
-            })
-        );
-    });
+    const { container } = render(
+      <IntlProvider locale="en" messages={{}}>
+        <ThemeToggleButton />
+      </IntlProvider>,
+    );
+    const checkbox = container.querySelector('#theme-toggle-checkbox');
+    checkbox.focus();
+    fireEvent.keyUp(checkbox, { key: 'Enter' });
+
+    expect(mockCookiesSet).toHaveBeenCalledWith(
+      'indigo-toggle-dark',
+      'dark',
+      expect.objectContaining({
+        domain: 'fake.url',
+        path: '/',
+        expires: expect.any(Date),
+      }),
+    );
+  });
 });
