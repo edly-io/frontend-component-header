fbfb76774afcd0d1fb79040aa661fa805f978002
arbirali <rahat.ali@arbisoft.com>
2025-05-14 12:20:06 +0500

    feat: enhance accessibility by adding descriptive label to theme toggle

--- src/ThemeToggleButton.jsx
+++ src/ThemeToggleButton.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { getConfig } from '@edx/frontend-platform';
 import Cookies from 'universal-cookie';
 import { Icon } from '@openedx/paragon';
@@ -8,6 +8,7 @@ const themeCookie = 'indigo-toggle-dark';
 const themeCookieExpiry = 90; // days
 
 const ThemeToggleButton = () => {
+  const [isDarkThemeEnabled, setIsDarkThemeEnabled] = useState(false);
   const cookies = new Cookies();
   const isThemeToggleEnabled = getConfig().INDIGO_ENABLE_DARK_TOGGLE;
 
@@ -61,10 +62,12 @@ const ThemeToggleButton = () => {
     if (cookies.get(themeCookie) === 'dark') {
       document.body.classList.remove('indigo-dark-theme');
       removeDarkThemeFromiframes();
+      setIsDarkThemeEnabled(false);
       theme = 'light';
     } else {
       document.body.classList.add('indigo-dark-theme');
       addDarkThemeToIframes();
+      setIsDarkThemeEnabled(true);
       theme = 'dark';
     }
     cookies.set(themeCookie, theme, getCookieOptions(serverURL));
@@ -86,6 +89,7 @@ const ThemeToggleButton = () => {
         <label htmlFor="theme-toggle-checkbox" className="switch">
           <input id="theme-toggle-checkbox" defaultChecked={cookies.get(themeCookie) === 'dark'} onChange={onToggleTheme} type="checkbox" />
           <span className="slider round" />
+          <span id="theme-label" className="sr-only">{`Switch to ${isDarkThemeEnabled ? 'Light' : 'Dark'} Mode`}</span>
         </label>
       </div>
       <div className="dark-theme-icon"><Icon src={Nightlight} /></div>
